8a89bb0dbe32a925a676c60ed0f9771d143b99e8
prchrbill <prchrbill@gmail.com>
2017-12-11 20:04:45 -0600

    Add files via upload

--- movie_website/entertainment_center.py
+++ movie_website/entertainment_center.py
@@ -2,41 +2,46 @@ import fresh_tomatoes   # importing to call it to build movie trailer website #
 import media            # contains the class, constructor, instance variables #
 
 # very entertaining - even though critics hated it #
-justice_league = media.Movie("Justice League", "Batman builds a team",
-                             "https://upload.wikimedia.org/wikipedia/commons" +
-                             "/8/8b/Justice_League_2017_film_logo.jpg",
-                             "https://www.youtube.com/watch?v=r9-DM9uBtVI")
+justice_league = media.Movie(
+    "Justice League", "Batman builds a team",
+    "https://upload.wikimedia.org/wikipedia/commons/8/8b/Justice_League_2017_film_logo.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=r9-DM9uBtVI"
+    )
 
 # best marvel movie - great challenge to the spiderman movie#
-thor_ragnarok = media.Movie("Thor Ragnarok", "Thor has to stop his Sister",
-                            "https://upload.wikimedia.org/wikipedia/commons/" +
-                            "4/44/Thor_Ragnarok.jpg",
-                            "https://www.youtube.com/watch?v=ue80QwXMRHg")
+thor_ragnarok = media.Movie(
+    "Thor Ragnarok", "Thor has to stop his Sister",
+    "https://upload.wikimedia.org/wikipedia/commons/4/44/Thor_Ragnarok.jpg",
+    "https://www.youtube.com/watch?v=ue80QwXMRHg")
 
 # best Wolverine movie ever#
-logan = media.Movie("Logan", "A mutant on the run tries to save his daughter.",
-                    "https://upload.wikimedia.org/wikipedia/en/3/37" +
-                    "/Logan_2017_poster.jpg",
-                    "https://www.youtube.com/watch?v=gbug3zTm3Ws")
+logan = media.Movie(
+    "Logan", "A mutant on the run tries to save his daughter.",
+    "https://upload.wikimedia.org/wikipedia/en/3/37/Logan_2017_poster.jpg",
+    "https://www.youtube.com/watch?v=gbug3zTm3Ws"
+    )
 
 # the wedding singer takes me back to relive my highschool years in music#
-wedding_singer = media.Movie("The Wedding Singer", "Man wants to get married.",
-                             "https://upload.wikimedia.org/wikipedia/en/8/84" +
-                             "/The_Wedding_Singer_film_poster.jpg",
-                             "https://www.youtube.com/watch?v=_bhU3NsCIDs")
+wedding_singer = media.Movie(
+    "The Wedding Singer", "Man wants to get married.",
+    "https://upload.wikimedia.org/wikipedia/en/8/84/The_Wedding_Singer_film_poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=_bhU3NsCIDs"
+    )
 
 # deadpool breaks the fourth wall and gives reynolds a good superhero movie #
-deadpool = media.Movie("Deadpool", "A mutant seeks revenge and to crack jokes",
-                       "https://upload.wikimedia.org/wikipedia/en/4/46" +
-                       "/Deadpool_poster.jpg",
-                       "https://www.youtube.com/watch?v=gtTfd6tISfw")
+deadpool = media.Movie(
+    "Deadpool", "A mutant seeks revenge and to crack jokes",
+    "https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg",
+    "https://www.youtube.com/watch?v=gtTfd6tISfw"
+    )
 
 # the lego batman movie was hysterical#
-lego_batman = media.Movie("The Lego Batman Movie", "A millionare " +
-                          "crime fighter learns he needs people.",
-                          "https://upload.wikimedia.org/wikipedia/en/6/6" +
-                          "1/The_Lego_Batman_Movie_PromotionalPoster.jpg",
-                          "https://www.youtube.com/watch?v=rGQUKzSDhrg")
+lego_batman = media.Movie(
+    "The Lego Batman Movie", "A millionare crime fighter learns"
+    "he needs people.",
+    "https://upload.wikimedia.org/wikipedia/en/6/61/The_Lego_Batman_Movie_PromotionalPoster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=rGQUKzSDhrg"
+    )
 
 
 movies = [justice_league, thor_ragnarok, logan, wedding_singer, deadpool,
--- movie_website/media.py
+++ movie_website/media.py
@@ -1,19 +1,20 @@
-import webbrowser #importing module to be used in our class
+import webbrowser  # importing module to be used in our class
 
-class Movie():  #creates way to store information about a movie, particularly the title,
-                #storyline, poster image and trailer
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        #This initializes the  Movie instance using the data provied by the
-        #program that calls for this class to be used. That data is then 
-        #assigned to variables in that program(in this case entertainment_center.py
+
+class Movie():  # creates way to store information about a movie,
+                # particularly the title,storyline, poster image and trailer
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        # This initializes the  Movie instance using the data provied by the
+        # program that calls for this class to be used. That data is then
+        # assigned to variables in that program(this time entertainment_center)
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    #This function will open the trailer for the self(movie). We have to import    
-    #the webbrowser module(begining of this file) for it to be used.                                
+    # This function will open the trailer for the self(movie). Have to import
+    # the webbrowser module(begining of this file) for it to be used.
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-    
